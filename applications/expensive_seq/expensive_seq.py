992f304395879577ccf500f4756d887898dc127d
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2020-05-05 12:56:31 -0700

    completes expensive sequence

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,5 +1,15 @@
+lookup = dict()
 def expensive_seq(x, y, z):
-    # Implement me
+     if x <= 0: 
+         return y + z
+     if x > 0:
+        value = lookup.get((x, y, z))
+        if value:
+            return value
+        else:
+            value = expensive_seq(x-1,y+1,z) + expensive_seq(x-2,y+2,z*2) + expensive_seq(x-3,y+3,z*3)
+            lookup[(x, y, z)] = value
+            return value
 
 if __name__ == "__main__":
     for i in range(10):
