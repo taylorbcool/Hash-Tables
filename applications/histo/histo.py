3ad8f2d9c9aaa5eaad38e7801164548ffcad03c8
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2020-05-05 15:56:12 -0700

    completes histo

--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -1,1 +1,29 @@
-# Implement me.
+ignored_chars = [
+        '"', ':', ';', ',', '.', '-', '+', '=', 
+        '/', '\\', '|', '[', ']', '{', '}', '(',
+         ')', '*', '^', '&'
+    ]
+
+histogram = dict()
+
+with open("robin.txt") as f:
+    words = f.read().split()
+    max_length = 0
+    for i, word in enumerate(words):
+        word = word.lower()
+        for char in ignored_chars:
+            word = word.replace(char, '')
+        if len(word) == 0:
+            continue
+        if len(word) > max_length:
+            max_length = len(word)
+        if histogram.get(word):
+            histogram[word] += '#'
+        else:
+            histogram[word] = '#'
+
+histogram = sorted(histogram.items(), 
+                   key=lambda h: (-len(h[1]), h[0]))
+
+for word, hashes in histogram:
+    print(word + (' ' * (max_length-len(word))) + '  ' + hashes)
