c87a194f2cfcf4c31fc95236484ef19a7d7f301e
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2020-05-05 17:11:16 -0700

    completes sumdiff

--- applications/sumdiff/sumdiff.py
+++ applications/sumdiff/sumdiff.py
@@ -4,11 +4,31 @@ f(a) + f(b) = f(c) - f(d)
 """
 
 #q = set(range(1, 10))
-#q = set(range(1, 200))
+# q = set(range(1, 200))
 q = (1, 3, 4, 7, 12)
 
 
 def f(x):
     return x * 4 + 6
 
-# TODO: Implement me.
+val_map = dict()
+
+for i, a in enumerate(q):
+    for b in q[i:]:
+        val = f(a) + f(b)
+        if val_map.get(val):
+            val_map[val].append((a, b))
+        else:
+            val_map[val] = [(a, b)]
+        if a != b:
+            val_map[val].append((b, a))
+
+
+q = list(reversed(q))
+for i, c in enumerate(q):
+    for d in q[i+1:]:
+        val = f(c) - f(d)
+        sums = val_map.get(val)
+        if sums:
+            for pair in sums:
+                print(f"f({pair[0]}) + f({pair[1]}) = f({c}) - f({d})")
